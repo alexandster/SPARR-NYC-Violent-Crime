f515607d59b753b9e00b03e97d75afacb2334a23
tilmandavies <tdavies@maths.otago.ac.nz>
2025-01-28 11:00:17 +1300

    jittering, monthly risks code and results

--- code/NYC Arrests.R
+++ code/NYC Arrests.R
@@ -115,3 +115,106 @@ plot(hvrr2org,main="(adaptive using unjittered data)")
 
 #save.image(file='../data/Arrests.RData')
 
+
+
+####### tilman's scratchpad ######
+
+load("data/Arrests.RData")
+
+library("geoR")  # for 'jitterDupCoords' function
+library("lubridate") # for 'month' and 'year'
+
+dfjit <- df
+set.seed(123) # Not strictly necessary, but gives the ability to replicate random jittering
+t1 <- Sys.time()
+jcoords <- jitterDupCoords(dfjit[,c("X_COORD_CD","Y_COORD_CD")],min=1,max=300)  # This only jitters duplicated coords. You could use the 'jitter2d' function to jitter *all* points
+t2 <- Sys.time()
+t2-t1 # this particular jitter takes around 90 secs
+dfjit[,c("X_COORD_CD","Y_COORD_CD")] <- jcoords
+
+
+# jittering will result in a small number of data points lost over the edge of the boundary. We need to identify these and remove them.
+sum(!inside.owin(x=dfjit$X_COORD_CD,y=dfjit$Y_COORD_CD,w=w)) # this many points fell outside
+dfjit <- dfjit[inside.owin(x=dfjit$X_COORD_CD,y=dfjit$Y_COORD_CD,w=w),] # keep only 'inside' points
+
+dfjit_0 <- subset(dfjit, class == 0) 
+dfjit_1 <- subset(dfjit, class == 1) 
+dfjit01 <- rbind(dfjit_0,dfjit_1)
+
+
+unique(year(dfjit01$date)) # just checking only 1 year is represented
+table(month(dfjit01$date)) # all months are represented
+dfjit01_monthly <- list() # per-month split of data
+for(i in 1:12) dfjit01_monthly[[i]] <- dfjit01[month(dfjit01$date)==i,]
+names(dfjit01_monthly) <- month.abb
+
+# creating separate monthly data lists for cases and controls
+dfjit0_monthly <- lapply(dfjit01_monthly,subset,subset=class==0)
+dfjit1_monthly <- lapply(dfjit01_monthly,subset,subset=class==1)
+
+# sanity check
+sapply(dfjit01_monthly,nrow)
+sapply(dfjit1_monthly,nrow)+sapply(dfjit0_monthly,nrow)  # all good here
+
+
+ppjit01_monthly <- list() # ppp objects of monthly cases and controls together (01)
+for(i in 1:12){
+  ppjit01_monthly[[i]] <- ppp(x=c(dfjit0_monthly[[i]]$X_COORD_CD,dfjit1_monthly[[i]]$X_COORD_CD),
+                              y=c(dfjit0_monthly[[i]]$Y_COORD_CD,dfjit1_monthly[[i]]$Y_COORD_CD),
+                              marks=factor(rep(c("control","case"),c(nrow(dfjit0_monthly[[i]]),nrow(dfjit1_monthly[[i]])))),
+                              window=w)
+}
+names(ppjit01_monthly) <- month.abb
+
+ppjit0_monthly <- ppjit1_monthly <- list() # ppp objects of monthly cases and controls separately
+for(i in 1:12){
+  ppsplit_temp <- split(ppjit01_monthly[[i]])
+  ppjit0_monthly[[i]] <- ppsplit_temp$control
+  ppjit1_monthly[[i]] <- ppsplit_temp$case
+}
+names(ppjit0_monthly) <- names(ppjit1_monthly) <- month.abb
+  
+sapply(ppjit0_monthly,OS) # around 3000 
+sapply(ppjit1_monthly,OS) # around 4000
+sapply(ppjit0_monthly,NS) # around 2800
+sapply(ppjit1_monthly,NS) # around 3500
+sapply(ppjit01_monthly,LSCV.risk,method="kelsall-diggle") #  around 900
+
+rr_monthly <- lapply(ppjit01_monthly,risk,h0=2000,tolerate=TRUE)
+
+par(mfrow=c(4,3))
+for(i in 1:12){
+  plot(rr_monthly[[i]],main=names(rr_monthly)[i],tol.show=FALSE)
+  tol.contour(rr_monthly[[i]]$P,levels=0.05,col="seagreen4",drawlabels=FALSE,add=TRUE,lwd=2)
+}
+
+
+
+# just playing with a fully continuous spatiotemporal rr
+range(as.numeric(dfjit$date))
+dfjit_0_pppst <- ppp(dfjit_0$X_COORD_CD, dfjit_0$Y_COORD_CD, marks=as.numeric(dfjit_0$date),window = w)
+dfjit_1_pppst <- ppp(dfjit_1$X_COORD_CD, dfjit_1$Y_COORD_CD, marks=as.numeric(dfjit_1$date),window = w)
+OS.spattemp(dfjit_0_pppst)
+OS.spattemp(dfjit_1_pppst)
+
+t1 <- Sys.time()
+stden0 <- spattemp.density(dfjit_0_pppst,h=2000,lambda=14,tlim=c(19350,19730))
+stden1 <- spattemp.density(dfjit_1_pppst,h=2000,lambda=14,tlim=c(19350,19730))
+t2 <- Sys.time()
+t2 - t1  # around 5 mins
+
+dev.new()
+plot(stden0,sleep=0.01)
+Sys.sleep(1)
+plot(stden1,sleep=0.01)
+dev.off()
+
+t1 <- Sys.time()
+strr <- spattemp.risk(stden1,stden0,tolerate=TRUE)
+t2 <- Sys.time()
+t2 - t1  # around 5 mins
+
+dev.new()
+plot(strr,sleep=0.1,fix.range=TRUE)
+dev.off()
+
