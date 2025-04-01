45891032e3dfbc53bc7841bf06e166ae35273956
Alexander Hohl <alexander.hohl84@gmail.com>
2025-04-01 16:23:08 -0600

    update code

--- code/NYC Arrests.R
+++ code/NYC Arrests.R
@@ -12,6 +12,7 @@ library(lubridate) # for 'month' and 'year'
 # set workspace----
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
+# load dataset
 load("../data/Arrests.RData")
 
 dfjit <- df
@@ -27,14 +28,13 @@ dfjit[,c("X_COORD_CD","Y_COORD_CD")] <- jcoords
 sum(!inside.owin(x=dfjit$X_COORD_CD,y=dfjit$Y_COORD_CD,w=w)) # this many points fell outside
 dfjit <- dfjit[inside.owin(x=dfjit$X_COORD_CD,y=dfjit$Y_COORD_CD,w=w),] # keep only 'inside' points
 
+# separate classes
 dfjit_0 <- subset(dfjit, class == 0) 
 dfjit_1 <- subset(dfjit, class == 1) 
 dfjit01 <- rbind(dfjit_0,dfjit_1)
 
-
-unique(year(dfjit01$date)) # just checking only 1 year is represented
-table(month(dfjit01$date)) # all months are represented
-dfjit01_monthly <- list() # per-month split of data
+# per-month split of data
+dfjit01_monthly <- list() 
 for(i in 1:12) dfjit01_monthly[[i]] <- dfjit01[month(dfjit01$date)==i,]
 names(dfjit01_monthly) <- month.abb
 
@@ -42,12 +42,8 @@ names(dfjit01_monthly) <- month.abb
 dfjit0_monthly <- lapply(dfjit01_monthly,subset,subset=class==0)
 dfjit1_monthly <- lapply(dfjit01_monthly,subset,subset=class==1)
 
-# sanity check
-sapply(dfjit01_monthly,nrow)
-sapply(dfjit1_monthly,nrow)+sapply(dfjit0_monthly,nrow)  # all good here
-
-
-ppjit01_monthly <- list() # ppp objects of monthly cases and controls together (01)
+# ppp objects of monthly cases and controls together (01)
+ppjit01_monthly <- list() 
 for(i in 1:12){
   ppjit01_monthly[[i]] <- ppp(x=c(dfjit0_monthly[[i]]$X_COORD_CD,dfjit1_monthly[[i]]$X_COORD_CD),
                               y=c(dfjit0_monthly[[i]]$Y_COORD_CD,dfjit1_monthly[[i]]$Y_COORD_CD),
@@ -56,7 +52,8 @@ for(i in 1:12){
 }
 names(ppjit01_monthly) <- month.abb
 
-ppjit0_monthly <- ppjit1_monthly <- list() # ppp objects of monthly cases and controls separately
+# ppp objects of monthly cases and controls separately
+ppjit0_monthly <- ppjit1_monthly <- list() 
 for(i in 1:12){
   ppsplit_temp <- split(ppjit01_monthly[[i]])
   ppjit0_monthly[[i]] <- ppsplit_temp$control
@@ -64,180 +61,76 @@ for(i in 1:12){
 }
 names(ppjit0_monthly) <- names(ppjit1_monthly) <- month.abb
 
-sapply(ppjit0_monthly,OS) # around 3000 http://127.0.0.1:9581/graphics/plot_zoom_png?width=1572&height=805
-sapply(ppjit1_monthly,OS) # around 4000
-sapply(ppjit0_monthly,NS) # around 2800
-sapply(ppjit1_monthly,NS) # around 3500
-sapply(ppjit01_monthly,LSCV.risk,method="kelsall-diggle") #  around 900
+# Compute risk surface for each month
+rr_monthly <- lapply(ppjit01_monthly,risk,h0=2000,tolerate=TRUE, res = 1500)
 
-rr_monthly <- lapply(ppjit01_monthly,risk,h0=2000,tolerate=TRUE, res = 750)
-
-#small multiples plot: monthly clusters
-dev.off()
-#par(mfrow=c(4,3))
+# small multiples plot: monthly clusters
 df_res = data.frame()
+
 for(i in 1:12){
   print(i)
-  plot(rr_monthly[[i]],main=names(rr_monthly)[i],tol.show=FALSE)
-  tol.contour(rr_monthly[[i]]$P,levels=0.05,col="seagreen4",drawlabels=FALSE,add=TRUE,lwd=2)
+  
+  #risk surface
+  r <- raster(rr_monthly[[i]]$rr)
+  crs(r) <- 2263
+  
+  #writeRaster(r, paste0("../output/rho_", i), format = "GTiff", overwrite=TRUE)
+  
+  # SPARR plotting
+  #plot(rr_monthly[[i]],main=names(rr_monthly)[i],tol.show=FALSE)
+  #tol.contour(rr_monthly[[i]]$P,levels=0.05,col="seagreen4",drawlabels=FALSE,add=TRUE,lwd=2)
+
+  # get tolerance contours  
   rho.class <- tol.classify(rr_monthly[[i]], cutoff = 0.05)
-  ID <- 1:length(rho.class[["finsplit"]])       #cluster identifier
-  Cases <- lengths(rho.class[["finsplit"]])     #case count
-  Controls <- lengths(rho.class[["ginsplit"]])  #control count
-  N <- Cases + Controls                         #point count
-  Risk <- Cases/N                               #
-  #contours to sf
+
+  # filter out clusters without cases
+  indices <- lengths(rho.class[["finsplit"]]) > 0 
+  rho.class[["finsplit"]] <- rho.class[["finsplit"]][indices]  #filter out clusters without cases
+  rho.class[["ginsplit"]] <- rho.class[["ginsplit"]][indices]  #filter out clusters without cases
+    
+  # cluster identifier
+  ID <- 1:length(rho.class[["finsplit"]])   
+  
+  # case count
+  Cases <- lengths(rho.class[["finsplit"]])
+  
+  # control count
+  Controls <- lengths(rho.class[["ginsplit"]])[1:length(rho.class[["finsplit"]])]
+  
+  # point count
+  N <- Cases + Controls
+  
+  # Risk
+  Risk <- Cases/N                             
+  
+  # contours to sf
   pcpolys <- rho.class$pcpolys %>%
     lapply(., FUN = st_as_sf) %>%
     do.call(rbind, .) %>%
     st_set_crs(2263)
+  
+  # filter polygons (clusters without cases)
+  pcpolys <- pcpolys[indices, ]
+  
+  # polygon ID
   pcpolys$ID <- 1:length(rho.class[["finsplit"]])
+  
+  # polygon month
   pcpolys$month <- i
-  st_write(pcpolys, paste0("../output/clusters_", i, ".shp"), append=FALSE)
+  
+  # write to file
+  #st_write(pcpolys, paste0("../output/clusters_", i, ".shp"), append=FALSE)
+  
+  # polygon area
   Area <- st_area(pcpolys) %>%
     units::set_units(., value = km^2) #Takes care of units
-  Case_density <- Cases/Area                    
+  
+  # case density
+  Case_density <- Cases/Area
+  
+  # attach to table
   df_res <- rbind(df_res, data.frame(i, ID, N, Cases, Controls, Risk, Case_density, Area))
   
 }
-#write.csv(df_res, "../output/clusters.csv", row.names = FALSE)
-
-# #read table
-# df <- read.csv("../data/NYPD_Arrests_Data__Historic__20241216.csv")   #source: https://s.cnmilf.com/user74170196/https/catalog.data.gov/dataset/nypd-arrest-data-year-to-date
-# df <- subset(df, Y_COORD_CD != 0 & ARREST_BORO == "M", select = c(ARREST_KEY, ARREST_DATE, OFNS_DESC, X_COORD_CD, Y_COORD_CD))
-# df<-  na.omit(df) 
-# 
-# #date format  
-# df$date <- as.Date(df$ARREST_DATE, "%m/%d/%Y")
-# 
-# #select 2023
-# df <- df %>%
-#   subset(., date >= "2023-01-01" & date <= "2023-12-31")
-# 
-# # time series plot
-# p <- ggplot(df, aes(x=date)) +
-#   geom_bar()
-# p
-# 
-# #offense frequency
-# table(df$OFNS_DESC)
-# 
-# #classify: offense
-# df$class <- ifelse(df$OFNS_DESC == "ASSAULT 3 & RELATED OFFENSES" | 
-#                      df$OFNS_DESC == "MURDER & NON-NEGL. MANSLAUGHTER" |
-#                      df$OFNS_DESC == "RAPE" |
-#                      df$OFNS_DESC == "ROBBERY"
-#                    , 1, 0)
-# 
-# #offense frequency
-# table(df$class)
-
-# #separate
-# df_0 <- subset(df, class == 0) 
-# df_1 <- subset(df, class == 1) 
-# 
-# #read NYC boundary
-# geom <- st_read("../data/manhattan_boundary.shp") %>%
-#   st_transform(., crs = 2263) %>%
-#   st_coordinates(.) %>%
-#   as.data.frame(.) 
-# 
-# #ppp object window
-# w <- owin(poly=list(x=rev(geom[, "X"]),y=rev(geom[, "Y"]))) #window
-# plot(w)
-# 
-# #ppp
-# df_0_ppp <- ppp(df_0$X_COORD_CD, df_0$Y_COORD_CD, window = w)
-# df_1_ppp <- ppp(df_1$X_COORD_CD, df_1$Y_COORD_CD, window = w)
-# 
-# # inspect raw data (with fixed window)
-# crppp <- ppp(x=c(df_0_ppp$x,df_1_ppp$x),
-#              y=c(df_0_ppp$y,df_1_ppp$y),
-#              marks=factor(rep(c("control","case"),c(npoints(df_0_ppp),npoints(df_1_ppp)))),
-#              window=w)
-# 
-# # create a slightly jittered version of the dataset (30-60 secs)
-# crjit <- rshift(crppp,group=factor(1:npoints(crppp)),radius=300,edge="none")
-# npoints(unique(unmark(crjit))) # Much better
-# 
-# #jittered vs. unjittered
-# par(mfrow=c(1,2))
-# plot(crppp,cex=0.5,main="unjittered")
-# plot(crjit,cex=0.5,main="jittered")
-# 
-# # get an idea of suggested bandwidths by running several different selectors
-# OS(crppp) # 1941.747 ftUS
-# NS(crjit) # 1790.382 ftUS
-# 
-# hk <- LSCV.risk(crjit,method="kelsall-diggle");hk #   402.0214 ftUS
-# hh <- LSCV.risk(crjit,method="hazelton");hh # 408.7517 ftUS
-# hd <- LSCV.risk(crjit,method="davies");hd  # 442.8772 ftUS
-# 
-# #################################################################
-# ##@Tilman: run from here
-# 
-# ##uncomment and run the line below
-# # load("../data/Arrests.RData")
-# #################################################################
-# 
-# # compute SPARR
-# hvrr1 <- risk(crjit,h0=hd,tolerate=TRUE)  #using the Davies bandwidth
-# hvrr2 <- risk(crjit,adapt=TRUE,h0=hd,tolerate=TRUE,pilot.symmetry="pooled", davies.baddeley=0.01) 
-# hvrr1org <- risk(crppp,h0=hd,tolerate=TRUE) # repeat the above using the original/unjittered data so we can compare the effect of the jittering
-# hvrr2org <- risk(crppp,adapt=TRUE,h0=hd,tolerate=TRUE,pilot.symmetry="pooled", davies.baddeley=0.01) 
-# 
-# #plot using Davies
-# par(mfrow=c(2,2))
-# plot(hvrr1,main="fixed; h=hd")
-# plot(hvrr2,main="symmetric (pooled) adaptive; h0=hp=hd; data jittered")
-# plot(hvrr1org,main="(fixed using unjittered data)")
-# plot(hvrr2org,main="(adaptive using unjittered data)")
-# 
-# # repeat these four estimates, using a larger bandwidth (NS bandwidth)
-# hvrr1 <- risk(crjit,h0=1790.382,tolerate=TRUE)  
-# hvrr2 <- risk(crjit,adapt=TRUE,h0=1790.382,tolerate=TRUE,pilot.symmetry="pooled", davies.baddeley=0.01) 
-# hvrr1org <- risk(crppp,h0=1790.382,tolerate=TRUE) # repeat the above using the original/unjittered data so we can compare the effect of the jittering
-# hvrr2org <- risk(crppp,adapt=TRUE,h0=1790.382,tolerate=TRUE,pilot.symmetry="pooled", davies.baddeley=0.01) 
-# 
-# #plot using larger bandwidth
-# par(mfrow=c(2,2))
-# plot(hvrr1,main="fixed; h=1790.382")
-# plot(hvrr2,main="symmetric (pooled) adaptive; h0=hp=1790.382; data jittered")
-# plot(hvrr1org,main="(fixed using unjittered data)")
-# plot(hvrr2org,main="(adaptive using unjittered data)")
-# 
-# #save.image(file='../data/Arrests.RData')
-
-
-
-####### tilman's scratchpad ######
-
-
-# # just playing with a fully continuous spatiotemporal rr
-# range(as.numeric(dfjit$date))
-# dfjit_0_pppst <- ppp(dfjit_0$X_COORD_CD, dfjit_0$Y_COORD_CD, marks=as.numeric(dfjit_0$date),window = w)
-# dfjit_1_pppst <- ppp(dfjit_1$X_COORD_CD, dfjit_1$Y_COORD_CD, marks=as.numeric(dfjit_1$date),window = w)
-# OS.spattemp(dfjit_0_pppst)
-# OS.spattemp(dfjit_1_pppst)
-# 
-# t1 <- Sys.time()
-# stden0 <- spattemp.density(dfjit_0_pppst,h=2000,lambda=14,tlim=c(19350,19730))
-# stden1 <- spattemp.density(dfjit_1_pppst,h=2000,lambda=14,tlim=c(19350,19730))
-# t2 <- Sys.time()
-# t2 - t1  # around 5 mins
-# 
-# dev.new()
-# plot(stden0,sleep=0.01)
-# Sys.sleep(1)
-# plot(stden1,sleep=0.01)
-# dev.off()
-# 
-# t1 <- Sys.time()
-# strr <- spattemp.risk(stden1,stden0,tolerate=TRUE)
-# t2 <- Sys.time()
-# t2 - t1  # around 5 mins
-# 
-# dev.new()
-# plot(strr,sleep=0.1,fix.range=TRUE)
-# dev.off()
+write.csv(df_res, "../output/clusters.csv", row.names = FALSE)
 
