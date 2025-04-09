8bd96d60aa0e9c0ef9c11d9fc6c80dce3336da6d
Alexander Hohl <alexander.hohl84@gmail.com>
2025-04-09 09:42:43 -0600

    update

--- code/NYC Arrests.R
+++ code/NYC Arrests.R
@@ -8,12 +8,40 @@ library(ggplot2)
 library(geoR)  # for 'jitterDupCoords' function
 library(lubridate) # for 'month' and 'year'
 
-
 # set workspace----
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-# load dataset
-load("../data/Arrests.RData")
+#read table
+df <- read.csv("../data/NYPD_Arrests_Data__Historic__20241216.csv")   #source: https://s.cnmilf.com/user74170196/https/catalog.data.gov/dataset/nypd-arrest-data-year-to-date
+df <- subset(df, Y_COORD_CD != 0 & ARREST_BORO == "M", select = c(ARREST_KEY, ARREST_DATE, OFNS_DESC, X_COORD_CD, Y_COORD_CD))
+df<-  na.omit(df) 
+
+#date format  
+df$date <- as.Date(df$ARREST_DATE, "%m/%d/%Y")
+
+#select 2023
+df <- df %>%
+  subset(., date >= "2023-01-01" & date <= "2023-12-31")
+
+df$month <- month(df$date)
+
+# time series plot
+p <- ggplot(df, aes(x=date)) +
+  geom_bar()
+p
+
+#offense frequency
+table(df$OFNS_DESC)
+
+#classify: offense
+df$class <- ifelse(df$OFNS_DESC == "ASSAULT 3 & RELATED OFFENSES" | 
+                     df$OFNS_DESC == "MURDER & NON-NEGL. MANSLAUGHTER" |
+                     df$OFNS_DESC == "RAPE" |
+                     df$OFNS_DESC == "ROBBERY"
+                   , 1, 0)
+
+#offense frequency
+table(df$class)
 
 dfjit <- df
 set.seed(123) # Not strictly necessary, but gives the ability to replicate random jittering
@@ -23,6 +51,15 @@ t2 <- Sys.time()
 t2-t1 # this particular jitter takes around 90 secs
 dfjit[,c("X_COORD_CD","Y_COORD_CD")] <- jcoords
 
+#read NYC boundary
+geom <- st_read("../data/manhattan_boundary.shp") %>%
+  st_transform(., crs = 2263) %>%
+  st_coordinates(.) %>%
+  as.data.frame(.) 
+
+#ppp object window
+w <- owin(poly=list(x=rev(geom[, "X"]),y=rev(geom[, "Y"]))) #window
+plot(w)
 
 # jittering will result in a small number of data points lost over the edge of the boundary. We need to identify these and remove them.
 sum(!inside.owin(x=dfjit$X_COORD_CD,y=dfjit$Y_COORD_CD,w=w)) # this many points fell outside
@@ -62,7 +99,7 @@ for(i in 1:12){
 names(ppjit0_monthly) <- names(ppjit1_monthly) <- month.abb
 
 # Compute risk surface for each month
-rr_monthly <- lapply(ppjit01_monthly,risk,h0=2000,tolerate=TRUE, res = 1500)
+rr_monthly <- lapply(ppjit01_monthly,risk,h0=2000,tolerate=TRUE, res = 500)
 
 # small multiples plot: monthly clusters
 df_res = data.frame()
@@ -79,15 +116,15 @@ for(i in 1:12){
   # SPARR plotting
   #plot(rr_monthly[[i]],main=names(rr_monthly)[i],tol.show=FALSE)
   #tol.contour(rr_monthly[[i]]$P,levels=0.05,col="seagreen4",drawlabels=FALSE,add=TRUE,lwd=2)
-
+  
   # get tolerance contours  
   rho.class <- tol.classify(rr_monthly[[i]], cutoff = 0.05)
-
+  
   # filter out clusters without cases
   indices <- lengths(rho.class[["finsplit"]]) > 0 
   rho.class[["finsplit"]] <- rho.class[["finsplit"]][indices]  #filter out clusters without cases
   rho.class[["ginsplit"]] <- rho.class[["ginsplit"]][indices]  #filter out clusters without cases
-    
+  
   # cluster identifier
   ID <- 1:length(rho.class[["finsplit"]])   
   
@@ -132,5 +169,4 @@ for(i in 1:12){
   df_res <- rbind(df_res, data.frame(i, ID, N, Cases, Controls, Risk, Case_density, Area))
   
 }
-write.csv(df_res, "../output/clusters.csv", row.names = FALSE)
-
+#write.csv(df_res, "../output/clusters.csv", row.names = FALSE)
